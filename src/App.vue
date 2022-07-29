be135e64dd0e3e82b7b66932fce274e1cfa8d73c
André Joarez <andre.ncardoso@hotmail.com>
2022-07-29 10:49:30 -0300

    Atualização IPCA 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    <calculadora-ipca />
   </div>
 </template>
 
@@ -20,12 +20,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Ipca/CalculadoraIpca.vue
+++ src/components/Ipca/CalculadoraIpca.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="app">
-        <h1>Tudo</h1>
+        <h1>Calculadora IPCA </h1>
+        <div class="input">
+            <label for="data">Data :</label>
+            <input type="text">
+            <label for="valor"> Valor :</label>
+            <input type="text">
+            <button class="corButton">Calcular</button>
+        </div> 
+        <div class="result">
+            <label for="result">Resultado   </label>
+            <input type="text">
+            <p></p>
+        </div>
     </div>
 </template>
 
@@ -14,5 +26,34 @@ export default{
 
 
 <style>
+    .app{
+        background: rgb(54, 50, 50); 
+    }
 
+    h1{
+        background: black;
+        color: white;
+        text-align: center;
+    }
+
+    .input {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+    }
+
+    .corButton{
+        background: blue;
+        color: white;
+    }
+
+    .result{
+        padding-bottom: 20px;
+        text-align:     center;
+        margin-top: 20px;
+        color: white;
+    }
 </style>
