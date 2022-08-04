a2147b32abf1391712deb5cfa340993dd40aaf86
André Joarez <andre.ncardoso@hotmail.com>
2022-08-04 16:47:40 -0300

    Adicionando calculos = IPCA e indice

--- src/components/Ipca/CalculadoraIpca.vue
+++ src/components/Ipca/CalculadoraIpca.vue
@@ -5,8 +5,8 @@
             <label for="data">Data :</label>
             <input type="text" v-model="data">
             <label for="valor"> Valor :</label>
-            <input type="text" v-model="test2">
-            <button class="corButton" @click="calculo()">Calcular</button>
+            <input type="text" v-model="valor">
+            <button class="corButton" @click="calcular()">Calcular</button>
         </div> 
         <div class="result">
             <label for="result">Resultado </label>
@@ -25,154 +25,178 @@ export default{
     data() {
         return {
             data: '',
-            test2: 0,
+            valor: 0,
             result: 0,
-            pegando: [],
         }
     },
     computed: {
         indices() {
             return indices;
-        }
+        },
+       
     },
 
     methods: {
-        calculo(){
-            const digitos = this.data.split('/')
-            const age = digitos[2]
-            const mounth = digitos [1]
-            const laco = mounth - 1
+        pick(age, mounth){
+            //let digitos = data.split('/')
+            //let age = digitos[2]
+            //let mounth = digitos [1]
+            let laco = mounth - 1
 
             if(mounth >= 1 && mounth <= 12){
                 if(age == 1994){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1994"][laco]
-                      //  total = this.indices["1994"][laco]
-                      //  this.result = total * 100
+                        return this.indices["1994"][laco]
                     }
                 }else if (age == 1995){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1995"][laco]
+                        return  this.indices["1995"][laco]
                     }
                 }else if(age == 1996){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1996"][laco]
+                        return  this.indices["1996"][laco]
                     }
                 }else if(age == 1997){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1997"][laco]
+                        return  this.indices["1997"][laco]
                     }
                 }else if(age == 1998){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1998"][laco]
+                        return  this.indices["1998"][laco]
                     }
                 }else if(age == 1999){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["1999"][laco]
+                        return  this.indices["1999"][laco]
                     }
                 }else if(age == 2000){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2000"][laco]
+                        return  this.indices["2000"][laco]
                     }
                 }else if(age == 2001){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2001"][laco]
+                        return  this.indices["2001"][laco]
                     }
                 }else if(age == 2002){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2002"][laco]
+                        return  this.indices["2002"][laco]
                     }
                 }else if(age == 2003){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2003"][laco]
+                        return  this.indices["2003"][laco]
                     }
                 }else if( age == 2004){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2004"][laco]
+                        return  this.indices["2004"][laco]
                     }
                 }else if(age == 2005){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2005"][laco]
+                        return  this.indices["2005"][laco]
                     }
                 }else if(age == 2006){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2006"][laco]
+                        return  this.indices["2006"][laco]
                     }
                 }else if(age == 2007){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2007"][laco]
+                        return  this.indices["2007"][laco]
                     }
                 }else if(age == 2008){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2008"][laco]
+                        return  this.indices["2008"][laco]
                     }
                 }else if(age == 2009){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2009"][laco]
+                        return  this.indices["2009"][laco]
                     }
                 }else if(age == 2010){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2010"][laco]
+                        return  this.indices["2010"][laco]
                     }
                 }else if(age == 2011){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2011"][laco]
+                        return  this.indices["2011"][laco]
                     }
                 }else if(age == 2012){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2012"][laco]
+                        return  this.indices["2012"][laco]
                     }
                 }else if(age == 2013){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2013"][laco]
+                        return  this.indices["2013"][laco]
                     }
                 }else if(age == 2014){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2014"][laco]
+                        return  this.indices["2014"][laco]
                     }
                 }else if(age == 2015){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2015"][laco]
+                        return  this.indices["2015"][laco]
                     }
                 }else if(age == 2016){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2016"][laco]
+                        return  this.indices["2016"][laco]
                     }
                 }else if(age == 2017){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2017"][laco]
+                        return  this.indices["2017"][laco]
                     }
                 }else if(age == 2018){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2018"][laco]
+                        return  this.indices["2018"][laco]
                     }
                 }else if(age == 2019){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2019"][laco]
+                        return  this.indices["2019"][laco]
                     }
                 }else if(age == 2020){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2020"][laco]
+                        return  this.indices["2020"][laco]
                     }
                 }else if(age == 2021){
                     for(laco; laco != parseFloat(mounth); laco++){
-                        this.result = this.indices["2021"][laco]
+                        return  this.indices["2021"][laco]
                     }
                 }else if(age == 2022){
                     if(mounth >= 1 && mounth <= 6){
                         for(laco; laco != parseFloat(mounth); laco++){
-                            this.result = this.indices["2022"][laco]
+                            return  this.indices["2022"][laco]
                         }
                     }else{
-                        this.result = "Mês incorreto!"
+                        return  "Mês incorreto!"
                     }
                 }else{
-                    this.result = "Ano incorreto"
+                    return  "Ano incorreto"
                 }
             }else{
                 this.result = "Mês incorreto!"
             }
-        }
+        },
+
+        calcular(){
+            const digitos = this.data.split('/')
+            let ano =  parseFloat(digitos[2])
+            let mes = parseFloat(digitos [1])
+            let total = 0
+
+            while(ano != 2023){
+                if(ano != 2022){
+                    for (mes; mes <= 12; mes++){
+                        let indice = this.pick(ano, mes)
+                        let total = parseFloat(this.valor) * (parseFloat(indice) / 100) + parseFloat(this.valor)
+                        this.valor = total
+                    }
+                }else if(ano == 2022){
+                    for (mes; mes <= 6; mes++){
+                        let indice = this.pick(ano, mes)
+                        let total = parseFloat(this.valor) * (parseFloat(indice) / 100) + parseFloat(this.valor)
+                        this.valor = total
+                    }
+                }
+                ano++
+                mes = 1
+            }
+            this.result = this.valor
+        },
     }
 }
 
