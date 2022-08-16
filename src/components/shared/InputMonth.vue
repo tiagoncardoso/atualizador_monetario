c5e74ff318ce8079a1aaaf6e23156ad3133f95e3
André Joarez <andre.ncardoso@hotmail.com>
2022-08-16 12:16:03 -0300

    Adicionando componente input

--- src/components/shared/InputMonth.vue
+++ src/components/shared/InputMonth.vue
@@ -1,50 +1,37 @@
 <template>
-    <v-row>
-        <v-col>
-            <v-menu
-                ref="menu"
-                v-model="menu"
-                :close-on-content-click="false"
-                :return-value.sync="date"
-                transition="scale-transition"
-                offset-y
-                max-width="290px"
-                min-width="auto"
-            >
-                <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                        v-model="dateFormatted"
-                        :label="label"
-                        hint="MM/YYYY"
-                        prepend-icon="mdi-calendar"
-                        readonly
-                        :outlined="outlined"
-                        :dense="dense"
-                        :background-color="backgroundColor"
-                        :color="color"
-                        v-bind="attrs"
-                        v-on="on"
-                    />
-                </template>
-                <v-date-picker
-                    v-model="date"
-                    type="month"
-                    no-title
-                    scrollable
-                    locale="pt-br"
-                >
-                    <v-spacer></v-spacer>
-                    <v-btn text color="primary" @click="menu = false">
-                        Cancelar
-                    </v-btn>
-                    <v-btn text color="primary" @click="$refs.menu.save(date)">
-                        OK
-                    </v-btn>
-                </v-date-picker>
-            </v-menu>
-            <v-spacer></v-spacer>
-        </v-col>
-    </v-row>
+  <v-row>
+    <v-col>
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        max-width="290px"
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="dateFormatted"
+            :label="label"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" type="month" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(date)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+      <v-spacer></v-spacer>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
