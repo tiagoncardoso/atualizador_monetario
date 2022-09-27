fee20b8f84a874fd2999061f77e13958856f34f1
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-09-27 10:04:39 -0300

    chore: Added backup do login

---
+++ src/views/login/main.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-main>
+            <login-secundary/>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import loginSecundary from './components/loginb/loginSecundary.vue';
+
+export default {
+    name: 'App',
+    components: { loginSecundary },
+    data: () => ({
+        currentItem: 'tab-Web',
+    }),
+};
+</script>
+
+<style scoped>
+div.v-toolbar__title {
+    margin: auto;
+    font-size: 25px;
+}
+</style>
