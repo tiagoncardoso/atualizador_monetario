bd81e4988f6c388e705ea271c65af5bfb417d7b4
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-09-30 10:50:46 -0300

    feat: Added layout main

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-    <div id="app">
+    <div>
         <router-view />
     </div>
 </template>
 
 <style lang="scss">
+
+.v-main{
+    padding: 32px 0px 0px 100px !important;
+}
 </style>
