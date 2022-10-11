6a5fb17383c2c81f794b9ac2f1d7c26b454a1aa1
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-10-11 09:32:18 -0300

    chore:removed tabs

--- src/views/calculadora/main.vue
+++ src/views/calculadora/main.vue
@@ -1,30 +1,7 @@
 <template>
     <v-app>
-        <v-main>
-            <v-toolbar color="#757575" dark>
-                <v-toolbar-title>CALCULADORA DE ATUALIZAÇÃO MONETÁRIA</v-toolbar-title>
-                <template #extension>
-                    <v-tabs 
-                    v-model="currentItem"
-                    fixed-tabs
-                    slider-color="white"
-                    background-color="#1867C0"
-                    pt-5
-                    >
-                        <v-tab href="#tabIpca"> IPCA (IBGE) </v-tab>
-                        <v-tab href="#tabInpc"> INPC (IBGE) </v-tab>
-                    </v-tabs>
-                </template>
-            </v-toolbar>
-
-            <v-tabs-items v-model="currentItem">
-                <v-tab-item value="tabIpca">
-                    <calculadora-principal calculadora="ipca"/>
-                </v-tab-item>
-                <v-tab-item value="tabInpc">
-                    <calculadora-principal calculadora="inpc"/>
-                </v-tab-item>
-            </v-tabs-items>
+        <v-main class="principal">
+            <calculadora-principal />
         </v-main>
     </v-app>
 </template>
@@ -44,8 +21,7 @@ export default {
 </script>
 
 <style scoped>
-div.v-toolbar__title {
-    margin: auto;
-    font-size: 25px;
+.principal{
+    margin-top: 50px !important;
 }
 </style>
