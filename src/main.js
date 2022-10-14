5d7611efd6adec55846233d2e1816b1f02ec8983
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-10-14 11:23:38 -0300

    Feat: "Added mask for input CPF"

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import './plugins/vuetify-money.js';
 import router from './router';
+import InputFacade from 'vue-input-facade';
 
 Vue.config.productionTip = false;
+Vue.use(InputFacade, {name: 'mask'});
 
 new Vue({
     vuetify,
