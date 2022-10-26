67a5303621c7457ea081b37d99e097b7e8d52b05
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-10-26 16:20:13 -0300

    Feat: Added API in usuario

--- src/store/modules/usuario.js
+++ src/store/modules/usuario.js
@@ -79,6 +79,38 @@ const actions = {
         let resp = await axios.get(`http://localhost:8000/api/${city}/cidades`);
         state.api.cidades = resp.data.cidades
     },
+    async saveUsuario({state}){
+        const payload = {
+            pessoa: {
+                nome: state.dadosPessoais.nome,
+                email: state.dadosPessoais.email,
+                outroEmail: state.dadosPessoais.outroEmail,
+                dataNascimento: state.dadosPessoais.dataNascimento,
+                genero: state.dadosPessoais.genero,
+                cpf: state.dadosPessoais.cpf,
+                rg: state.dadosPessoais.rg,
+                ufEmissor: state.dadosPessoais.ufEmissor,
+            },
+        
+            contato: {
+                logradouro: state.dadosContato.logradouro,
+                numero: state.dadosContato.numero,
+                complemento: state.dadosContato.complemento,
+                bairro: state.dadosContato.bairro,
+                cep: state.dadosContato.cep,
+                uf: state.dadosContato.uf,
+                cidade: state.dadosContato.cidade,
+                telefone: state.dadosContato.telefone,
+                email: state.dadosContato.email,
+            },
+        
+            usuario: {
+                usuario: state.dadosUsuario.usuario,
+                senha: state.dadosUsuario.senha,
+            },
+        };
+        return await axios.post('http://localhost:8000/api/usuario', payload);
+    },
 };
 
 export default {
--- src/views/usuario/main.vue
+++ src/views/usuario/main.vue
@@ -3,9 +3,16 @@
         <dados-pessoais />
         <dados-contato />
         <dados-usuario />
+        <v-btn 
+            class="botao"
+            elevation="2"
+            color="primary"
+            @click="saveUsuario()"
+        >Salvar</v-btn>
     </v-container>
 </template>
 <script>
+import { mapActions } from 'vuex';
 import DadosContato from './dadosContato.vue';
 import DadosPessoais from './dadosPessoais.vue';
 import DadosUsuario from './dadosUsuario.vue';
@@ -14,7 +21,17 @@ export default {
     name: 'CadastroUsuario',
     components: { DadosContato, DadosPessoais, DadosUsuario },
     data: () => ({}),
+    methods: {
+        ...mapActions('usuario', ['saveUsuario'])
+    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.botao{
+    text-align: center !important;
+    margin-top: 10px;
+    justify-content: center !important;
+}
+</style>
